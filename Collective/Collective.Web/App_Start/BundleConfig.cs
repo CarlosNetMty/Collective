c73d3225454afc549dceb5ac205739c44d6c1627
Carlos Martinez <carlos.net@live.com>
2013-11-04 16:11:31 -0600

    EntityFramework Downgrade

--- Collective/Collective.Web/App_Start/BundleConfig.cs
+++ Collective/Collective.Web/App_Start/BundleConfig.cs
@@ -8,36 +8,76 @@ namespace Collective.Web
         // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            // ****************  Styles  ****************
+            bundles.Add(new StyleBundle("~/bundles/styles")
+                .Include("~/content/normalize.css",
+                        "~/content/main.css",
+                        "~/content/jnotify.css"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
-                        "~/Scripts/jquery-ui-{version}.js"));
+            bundles.Add(new StyleBundle("~/bundles/css")
+                .Include("~/content/foundation.css",
+                        "~/content/jquery-te.css",
+                        "~/content/jnotify.css"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.unobtrusive*",
-                        "~/Scripts/jquery.validate*"));
+            // ****************  Scripts  ****************
+            bundles.Add(new ScriptBundle("~/bundles/scripts")
+                .Include("~/scripts/framework/modernizr-2.6.2.js", 
+                        "~/scripts/framework/jquery-1.10.2.js"));
 
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+            bundles.Add(new ScriptBundle("~/bundles/framework")
+                .Include("~/scripts/framework/jquery.validate.js",
+                        "~/scripts/framework/jquery.validate.unobtrusive.js",
+                        "~/scripts/framework/knockout-2.3.0.js",
+                        "~/scripts/framework/jquery.linq.js",
+                        "~/scripts/framework/jquery.slabtext.js",
+                        "~/scripts/framework/json2.js",
+                        "~/scripts/framework/jstorage.js",
+                        "~/scripts/framework/jquery.timers.js",
+                        "~/scripts/framework/plugins.js",
+                        "~/scripts/framework/jquery.fancybox.pack.js",
+                        "~/scripts/framework/jquery.isotope.min.js",
+                        "~/scripts/framework/foundation.js",
+                        "~/scripts/framework/jquery-te-1.4.0.min.js",
+                        "~/scripts/framework/jNotify.jquery.js",
+                        "~/scripts/framework/jquery.noty.js",
+                        "~/scripts/framework/jquery.md5.js",
+                        "~/scripts/namespace.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
-
-            bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
-                        "~/Content/themes/base/jquery.ui.core.css",
-                        "~/Content/themes/base/jquery.ui.resizable.css",
-                        "~/Content/themes/base/jquery.ui.selectable.css",
-                        "~/Content/themes/base/jquery.ui.accordion.css",
-                        "~/Content/themes/base/jquery.ui.autocomplete.css",
-                        "~/Content/themes/base/jquery.ui.button.css",
-                        "~/Content/themes/base/jquery.ui.dialog.css",
-                        "~/Content/themes/base/jquery.ui.slider.css",
-                        "~/Content/themes/base/jquery.ui.tabs.css",
-                        "~/Content/themes/base/jquery.ui.datepicker.css",
-                        "~/Content/themes/base/jquery.ui.progressbar.css",
-                        "~/Content/themes/base/jquery.ui.theme.css"));
+            bundles.Add(new ScriptBundle("~/bundles/application").Include(
+                        // ****************  Common  ****************
+                        "~/scripts/utils/Storage.js",
+                        "~/scripts/utils/Global.js",
+                        "~/scripts/utils/Translations.js",
+                        "~/scripts/utils/Utileries.js",
+                        // ****************  Home  ****************
+                        "~/scripts/sales/Index.js",
+                        "~/scripts/home/Index.js",
+                        "~/scripts/home/Header.js",
+                        "~/scripts/home/Footer.js",
+                        "~/scripts/home/Gallery.js",
+                        "~/scripts/home/Detail.js",
+                        "~/scripts/home/Login.js",
+                        "~/scripts/home/About.js",
+                        "~/scripts/home/Contact.js",
+                        "~/scripts/home/Conditions.js",
+                        // ****************  Admin  ****************
+                        "~/scripts/admin/Content.js",
+                        "~/scripts/admin/Header.js",
+                        "~/scripts/admin/Sidebar.js",
+                        "~/scripts/admin/Index.js",
+                        "~/scripts/admin/Artists.js",
+                        "~/scripts/admin/ArtistsDetail.js",
+                        "~/scripts/admin/Users.js",
+                        "~/scripts/admin/UsersDetail.js",
+                        "~/scripts/admin/Products.js",
+                        "~/scripts/admin/ProductsDetail.js",
+                        "~/scripts/admin/Cover.js",
+                        // ****************  Admin  ****************                        
+                        "~/scripts/viewmodels/Collection.js",
+                        "~/scripts/viewmodels/Sale.js",
+                        "~/scripts/viewmodels/Footer.js",
+                        "~/scripts/viewmodels/User.js",
+                        "~/scripts/viewmodels/Artist.js"));
         }
     }
 }
